ecd4ae17079df9a2be913300819dd28bcdea4e52
Attahiru Kamba <bappahmuhammad18@gmail.com>
2024-08-10 01:15:35 +0100

    :fire: removed env_loader module

--- libs/boot.js
+++ libs/boot.js
@@ -1,7 +1,4 @@
-import envLoader from '../utils/env_loader';
-
 const startServer = (api) => {
-  envLoader();
   const port = process.env.PORT || 5000;
   const env = process.env.npm_lifecycle_event || 'dev';
   api.listen(port, () => {
